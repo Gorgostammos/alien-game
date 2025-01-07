af14b735d46c64e21d2bd62b6b70e1902abc39cd
GergesTammo2001 <gorgos2002@icloud.com>
2025-01-07 16:53:42 +0100

    endret på farget

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_farge = (153, 204, 255)
+        self.bg_farge = (14,26,79)
 
         # Ship settings.
         self.ship_speed = 1.5
